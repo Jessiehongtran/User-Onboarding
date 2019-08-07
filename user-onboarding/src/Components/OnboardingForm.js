30ca11d55913706e00587230688305daf5efaf95
Your Name <you@example.com>
2019-08-07 16:51:18 -0400

    setup Validations

--- user-onboarding/src/Components/OnboardingForm.js
+++ user-onboarding/src/Components/OnboardingForm.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import axios from 'axios'
-import {Form, Field, withFormik} from 'formik'
+import {Form, Field, withFormik, yupToFormErrors, ErrorMessage} from 'formik'
+import * as Yup from "yup";
 
-const OnboardingForm = () => {
+const OnboardingForm = ({touched, errors}) => {
 
     return (
         <div className="onboarding-form">
             <h1>User Onboarding Form</h1>
             <Form>
                 <Field type="text" name="name" placeholder="Your Name" />
+                {touched.name && errors.name && <p className="error">{errors.name}</p>}
+
                 <Field type="text" name="email" placeholder="Your Email" />
+                {touched.email && errors.email && <p className="error">{errors.email}</p>}
+
                 <Field type="text" name="password" placeholder="Your Password" />
+                {touched.password && errors.password && <p className="error">{errors.password}</p>}
+
                 <button>Submit</button>
             </Form>
         </div>
@@ -26,6 +33,12 @@ const FormikOnboarding = withFormik({
             password: values.password || ''
         }
     },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string().required('Email is required'),
+        password: Yup.string().min(8).required('Password has to contain at least 8 letters')
+    }),
 
     handleSubmit(values){
         console.log('submit', values)
