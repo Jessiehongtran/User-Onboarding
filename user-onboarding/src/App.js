fa1172d97437a0ced04285af4b628db34e8b9280
Your Name <you@example.com>
2019-08-07 16:05:40 -0400

    Created form component and imported into App

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import OnboardingForm from './Components/OnboardingForm'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Onboarding App</h1>
+    <OnboardingForm />
+    </div>
+  );
+}
+
+export default App;
